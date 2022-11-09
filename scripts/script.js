2e7fce689ad7f98c6edfa5fc587393ad9899b943
pealmus <leon.lucy1997@web.de>
2022-11-09 10:27:34 +0100

    Script

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+function speichern() {
+    let kommentare = document.getElementById("kommentare").value;
+    let inhalt = document.getElementById('inhalt');
+    window.localStorage.setItem('kommentare','inhalt');
+    console.log(kommentare);
+    let ausgabe = "";
+    for (let i =0; i < localStorage.length; i++) {
+        ausgabe += localStorage.key(i) + " = " + localStorage.getItem(localStorage.key(i)) + "<br>";
+    }
+    document.getElementById("comments").innerHTML = ausgabe;
+    document.getElementById("kom").innerHTML = Date();
+    document.getElementById("comments").innerHTML = kommentare;
+}
+
+
