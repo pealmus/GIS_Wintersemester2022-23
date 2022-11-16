c64a83057ceb3096f4806fc2f41702a0397f4d11
pealmus <leon.lucy1997@web.de>
2022-11-16 12:13:17 +0100

    Anpassungen:
    Neue Linkstruktur
    Video mit JavaScript eingebunden

--- scripts/script.js
+++ scripts/script.js
@@ -16,4 +16,42 @@ function speichern() {
     document.getElementById("comments").innerHTML = kommentare;
 }
 
+const tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+let player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        videoId: 'jNQXAC9IVRw',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+let done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+function stopVideo() {
+    player.stopVideo();
+}
+
+
+
+
+
+
+
 
